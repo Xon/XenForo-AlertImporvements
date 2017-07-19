12ba63cc18e753f2f176a90b4da5e59c0d9cdbac
Xon <tazipper1@hotmail.com>
2017-07-20 03:00:16 +0800

    Support report comments & conversation messages Like summarization

---
+++ upload/library/SV/AlertImprovements/SV/ConversationImprovements/AlertHandler/ConversationMessage.php
@@ -0,0 +1,45 @@
+<?php
+
+class SV_AlertImprovements_SV_ConversationImprovements_AlertHandler_ConversationMessage extends XFCP_SV_AlertImprovements_SV_ConversationImprovements_AlertHandler_ConversationMessage
+{
+    public function canSummarize(array $alert)
+    {
+        return $alert['action'] == 'like';
+    }
+
+    public function consolidateAlert(&$contentType, &$contentId, array $item)
+    {
+        switch($contentType)
+        {
+            case 'conversation_message':
+                return true;
+            default:
+                return false;
+        }
+    }
+
+    public function summarizeAlerts(array $summaryAlert, array $alerts, $groupingStyle)
+    {
+        if ($groupingStyle != 'content')
+        {
+            return null;
+        }
+
+        $summaryAlert['extra_data']['likes']['post'] = count($alerts);
+        $summaryAlert['action'] = 'like_summary';
+
+        return $summaryAlert;
+    }
+
+    protected function _prepareLike_summary(array $item, array $viewingUser)
+    {
+        if (is_callable('parent::_prepareLike_summary'))
+        {
+            $item = parent::_prepareLike_summary($item, $viewingUser);
+        }
+
+        $item["isSummary"] = true;
+
+        return SV_AlertImprovements_Helper::prepareRateSummary($item, $viewingUser);
+    }
+}
