d392bdbaa16830aa0464578f3db3f70718ae3414
Xon <tazipper1@hotmail.com>
2017-09-17 08:46:21 +0800

    enforce requirements in installer

--- upload/library/SV/AlertImprovements/Installer.php
+++ upload/library/SV/AlertImprovements/Installer.php
@@ -5,6 +5,19 @@ class SV_AlertImprovements_Installer
     public static function install($existingAddOn, $addOnData)
     {
         $version = isset($existingAddOn['version_id']) ? $existingAddOn['version_id'] : 0;
+        $required = '5.4.0';
+        $phpversion = phpversion();
+        if (version_compare($phpversion, $required, '<'))
+        {
+            throw new XenForo_Exception(
+                "PHP {$required} or newer is required. {$phpversion} does not meet this requirement. Please ask your host to upgrade PHP",
+                true
+            );
+        }
+        if (XenForo_Application::$versionId < 1030070)
+        {
+            throw new Exception('XenForo 1.3.0+ is Required!');
+        }
 
         SV_Utils_Install::addColumn(
             'xf_user_option', 'sv_alerts_page_skips_mark_read', 'tinyint(3) unsigned NOT NULL DEFAULT 0'
