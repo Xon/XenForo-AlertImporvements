4b1789afea07208a6e3a2fcb9091cffb7028e11b
Xon <tazipper1@hotmail.com>
2018-04-04 05:32:49 +0800

    Support Tickets 1.6.x

--- upload/library/SV/AlertImprovements/NixFifty/Tickets/ControllerPublic/Ticket.php
+++ upload/library/SV/AlertImprovements/NixFifty/Tickets/ControllerPublic/Ticket.php
@@ -5,13 +5,26 @@ class SV_AlertImprovements_NixFifty_Tickets_ControllerPublic_Ticket extends XFCP
     public function actionView()
     {
         $response = parent::actionView();
-        if ($response instanceof XenForo_ControllerResponse_View && !empty($response->params['messages']))
+        if ($response instanceof XenForo_ControllerResponse_View)
         {
+            if (!empty($response->subView->params['messages']) )
+            {
+                $messages = $response->subView->params['messages'];
+            }
+            else if (!empty($response->params['messages']) )
+            {
+                $messages = $response->params['messages'];
+            }
+            else
+            {
+                $messages =  null;
+            }
+
             $visitor = XenForo_Visitor::getInstance();
-            if ($visitor->getUserId() && $visitor->alerts_unread)
+            if ($messages && $visitor->getUserId() && $visitor->alerts_unread)
             {
                 $alertModel = $this->_getAlertModel();
-                $contentIds = XenForo_Application::arrayColumn($response->params['messages'], 'message_id');
+                $contentIds = XenForo_Application::arrayColumn($messages, 'message_id');
                 $alertModel->markAlertsAsRead('ticket_message', $contentIds);
                 $contentIds = [$response->params['ticket']['ticket_id']];
                 $alertModel->markAlertsAsRead('ticket', $contentIds);
